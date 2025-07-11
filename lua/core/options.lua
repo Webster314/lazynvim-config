0e2ec96b28433b1ac24686a950167c46a489bf13
Anderson <jedistipeee@gmail.com>
2025-07-12 00:43:52 +0800

    first commit

---
+++ lua/core/options.lua
@@ -0,0 +1,6 @@
+vim.o.number = true          -- 显示行号
+vim.o.relativenumber = true  -- 相对行号
+vim.o.tabstop = 4            -- Tab 宽度
+vim.o.shiftwidth = 4         -- 缩进宽度
+vim.o.expandtab = true       -- Tab 转空格
+vim.o.colorcolumn = "80"     -- 颜色80列
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,4 @@
+return {
+  { import = "plugins.modules.ui.lualine" },
+}
+
