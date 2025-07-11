c69deee4ffc485f9d3d1abbda0f7b7e4a545e967
Anderson <jedistipeee@gmail.com>
2025-07-12 03:45:28 +0800

    disable netrw for using nvim-tree

--- lua/plugins/explorer/nvim-tree.lua
+++ lua/plugins/explorer/nvim-tree.lua
@@ -3,6 +3,13 @@ return {
     version = "*",
     lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons", },
+    init = function()
+        -- disable netrw at the very start of your init.lua
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        -- optionally enable 24-bit colour
+        vim.opt.termguicolors = true       
+    end,
     config = function()
         require("nvim-tree").setup {}
     end,
